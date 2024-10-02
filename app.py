69fdcf072555e5d6e4bb2c931ab0f403d581038a
DavidDu2001 <du.david@hotmail.com>
2024-10-02 15:59:11 +0200

    updated HTML file Added requirements.txt Added Deepl Key input

--- app.py
+++ app.py
@@ -1,58 +1,80 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-import os
-import subprocess
-import torch
-import deepl
-from googletrans import Translator
-from datetime import timedelta
-from whisper_trans import translate_whisper
-auth_key = "d36d6d0f-3a89-4a01-9437-7da88f7ac1c8:fx"
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash
+import os, shutil
+from whisper_trans import translate_whisper, valid_key
+
+# auth_key = "d36d6d0f-3a89-4a01-9437-7da88f7ac1c8:fx"
 
 app = Flask(__name__)
 
 # Set the directory where files will be stored
-UPLOAD_FOLDER = r'C:\Users\david\PycharmProjects\whisper\uploads'
+UPLOAD_FOLDER = r'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.secret_key = 'super secret key'
 # Ensure the upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 app.config['MAX_CONTENT_LENGTH'] = 5000 * 5000
-app.config['UPLOAD_EXTENSIONS'] = ['.mp3', '.mp4']
+app.config['UPLOAD_EXTENSIONS'] = ['mp3', 'mp4']
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+def delete_folder():
+    # Remove all files in the upload folder
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+
+
+
 @app.route('/', methods=['POST'])
 def upload_file():
-    uploaded_file = request.files['file']
-    model_size = request.form['model_size']  # Get the selected model size
-    deepl_key = request.form['deepl_key']
-    if uploaded_file and uploaded_file.filename != '':
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        uploaded_file.save(file_path)
+    delete_folder()
+    uploaded_file = request.files.get('file')
+    model_size = request.form.get('model_size')
+    deepl_key = request.form.get('DeepL_Key')
 
-        # Translate the file and get the new filename
+
+    file_name_without_extension = uploaded_file.filename.rsplit('.', 1)[1]
+    print(file_name_without_extension)
+    if not uploaded_file or uploaded_file.filename == '':
+        flash('No file selected. Please choose a valid audio file.')
+        return redirect(url_for('index'))
+
+    if file_name_without_extension not in app.config['UPLOAD_EXTENSIONS']:
+        flash('Wrong file type. Please choose a valid audio file.')
+        return redirect(url_for('index'))
+
+    if not valid_key(deepl_key):
+        flash('Invalid key.')
+        return redirect(url_for('index'))
+
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+    uploaded_file.save(file_path)
+
+    # Translate the file and get the new filename
+    try:
         filename = translate_whisper(file_path, app.config['UPLOAD_FOLDER'], model_size, deepl_key)
-        os.remove(file_path)
-        # Redirect to download the new SRT file
-        return redirect(url_for('download_file', filename=filename))
-    # Iterate over all files in the directory
-    folder_path = r"C:\Users\david\PycharmProjects\whisper\uploads"
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        # Check if it is a file (and not a directory)
-        if os.path.isfile(file_path):
-            os.remove(file_path)  # Delete the file
-    return redirect(url_for('index'))
+    except Exception as e:
+        flash(f'Error during processing: {str(e)}')
+        os.remove(file_path)  # Clean up uploaded file on error
+        return redirect(url_for('index'))
+
+    return redirect(url_for('download_file', filename=filename))
 
 
 @app.route('/downloads/<filename>')
 def download_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    # Send the file
+    response = send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
--- whisper_trans.py
+++ whisper_trans.py
@@ -1,47 +1,48 @@
-import subprocess
 import os
 import deepl
 import whisper
 import torch
-from googletrans import Translator
 
 from datetime import timedelta
-
+# deepl_key = "d36d6d0f-3a89-4a01-9437-7da88f7ac1c8:fx"
 
 def whisper_transcribe(file, output, model_size):
     # Configuration
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    try:
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    # Load the model
-    model = whisper.load_model(model_size).to(device)
+        # Load the model
+        model = whisper.load_model(model_size).to(device)
 
-    # Your audio file
+        # Your audio file
 
-    # Transcribe the audio
-    result = model.transcribe(file)
+        # Transcribe the audio
+        result = model.transcribe(file)
 
-    # Create SRT file
-    srt_file_path = os.path.join(output,
-                                 os.path.basename(file).replace('.mp3', '.srt'))  # Adjust the extension as needed
+        # Create SRT file
+        srt_file_path = os.path.join(output,
+                                     os.path.basename(file).replace('.mp3', '.srt'))  # Adjust the extension as needed
 
-    segments = result['segments']
+        segments = result['segments']
 
-    with open(srt_file_path, 'w', encoding='utf-8') as srtFile:
-        pass  # Clear the file
+        with open(srt_file_path, 'w', encoding='utf-8') as srtFile:
+            pass  # Clear the file
 
-    for segment in segments:
-        start_time = str(timedelta(seconds=segment['start'])).replace('.', ',')
-        end_time = str(timedelta(seconds=segment['end'])).replace('.', ',')
-        text = segment['text'].lstrip()  # Remove leading space
-        segment_id = segment['id'] + 1
+        for segment in segments:
+            start_time = str(timedelta(seconds=segment['start'])).replace('.', ',')
+            end_time = str(timedelta(seconds=segment['end'])).replace('.', ',')
+            text = segment['text'].lstrip()  # Remove leading space
+            segment_id = segment['id'] + 1
 
-        segment_text = f"{segment_id}\n{start_time} --> {end_time}\n{text}\n\n"
+            segment_text = f"{segment_id}\n{start_time} --> {end_time}\n{text}\n\n"
 
-        with open(srt_file_path, 'a', encoding='utf-8') as srtFile:
-            srtFile.write(segment_text)
+            with open(srt_file_path, 'a', encoding='utf-8') as srtFile:
+                srtFile.write(segment_text)
 
-    print(f"SRT file created at: {srt_file_path}")
-    return srt_file_path
+        print(f"SRT file created at: {srt_file_path}")
+        return srt_file_path
+    except Exception as e:
+        return e
 
 
 def convert_srt_txt(file):
@@ -75,7 +76,7 @@ def convert_txt_srt(file):
     return new_file
 
 
-def translate(file, file_trans, deepl_key):
+def translate(file, file_trans,deepl_key):
     filename = os.path.basename(file)
     translator = deepl.Translator(deepl_key)
 
@@ -93,8 +94,15 @@ def translate(file, file_trans, deepl_key):
 
     return output  # Return the translated file path
 
+def valid_key(key):
+    try:
+        translator = deepl.Translator(key)
+        translator.translate_text("test", target_lang="EN-US")
+        return True
+    except:
+        return False
 
-def translate_whisper(file, output, model_size, deepl_key):
+def translate_whisper(file, output, model_size,deepl_key):
     # Step 1: Transcribe the audio to SRT
     srt_file = whisper_transcribe(file, output,model_size)
 
@@ -102,7 +110,7 @@ def translate_whisper(file, output, model_size, deepl_key):
     txt_file = convert_srt_txt(srt_file)
 
     # Step 3: Translate the TXT file
-    translated_txt_file = translate(txt_file, output, deepl_key)
+    translated_txt_file = translate(txt_file, output,deepl_key)
 
     # Step 4: Convert the translated TXT back to SRT
     final_srt_file = convert_txt_srt(translated_txt_file)
